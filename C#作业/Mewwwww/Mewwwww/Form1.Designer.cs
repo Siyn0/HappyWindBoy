b9e5ac6447f7da61159dea9e8336163f52f635db
Siyn0 <36985807+Siyn0@users.noreply.github.com>
2021-07-05 16:20:53 +0800

    一个聊天机器人，使用了网上的聊天机器人api

---
+++ C#作业/Mewwwww/Mewwwww/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Mewwwww
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.CatName = new System.Windows.Forms.Label();
+            this.More = new System.Windows.Forms.Button();
+            this.Say = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.ChatList = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // CatName
+            // 
+            this.CatName.AutoSize = true;
+            this.CatName.Font = new System.Drawing.Font("黑体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CatName.ForeColor = System.Drawing.Color.Black;
+            this.CatName.Location = new System.Drawing.Point(3, 9);
+            this.CatName.Name = "CatName";
+            this.CatName.Size = new System.Drawing.Size(118, 24);
+            this.CatName.TabIndex = 0;
+            this.CatName.Text = "< Mewwwww";
+            // 
+            // More
+            // 
+            this.More.FlatAppearance.BorderSize = 0;
+            this.More.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.More.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.More.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.More.Font = new System.Drawing.Font("黑体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.More.ForeColor = System.Drawing.Color.Black;
+            this.More.Location = new System.Drawing.Point(306, 3);
+            this.More.Name = "More";
+            this.More.Size = new System.Drawing.Size(40, 36);
+            this.More.TabIndex = 1;
+            this.More.Text = "…";
+            this.More.UseVisualStyleBackColor = true;
+            // 
+            // Say
+            // 
+            this.Say.AcceptsReturn = true;
+            this.Say.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Say.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Say.Location = new System.Drawing.Point(26, 502);
+            this.Say.MaxLength = 300;
+            this.Say.MinimumSize = new System.Drawing.Size(250, 40);
+            this.Say.Multiline = true;
+            this.Say.Name = "Say";
+            this.Say.Size = new System.Drawing.Size(250, 40);
+            this.Say.TabIndex = 0;
+            // 
+            // Send
+            // 
+            this.Send.BackColor = System.Drawing.Color.LightGray;
+            this.Send.FlatAppearance.BorderSize = 0;
+            this.Send.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.Send.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.Send.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Send.Font = new System.Drawing.Font("黑体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Send.ForeColor = System.Drawing.Color.Black;
+            this.Send.Location = new System.Drawing.Point(284, 502);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(62, 40);
+            this.Send.TabIndex = 1;
+            this.Send.Text = "发送";
+            this.Send.UseVisualStyleBackColor = false;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // ChatList
+            // 
+            this.ChatList.AcceptsTab = true;
+            this.ChatList.BackColor = System.Drawing.Color.Silver;
+            this.ChatList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ChatList.CausesValidation = false;
+            this.ChatList.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ChatList.Location = new System.Drawing.Point(26, 42);
+            this.ChatList.MaximumSize = new System.Drawing.Size(320, 436);
+            this.ChatList.MinimumSize = new System.Drawing.Size(320, 436);
+            this.ChatList.Name = "ChatList";
+            this.ChatList.ReadOnly = true;
+            this.ChatList.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.ChatList.Size = new System.Drawing.Size(320, 436);
+            this.ChatList.TabIndex = 2;
+            this.ChatList.Text = "";
+            // 
+            // Home
+            // 
+            this.AcceptButton = this.Send;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(352, 551);
+            this.Controls.Add(this.ChatList);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.Say);
+            this.Controls.Add(this.More);
+            this.Controls.Add(this.CatName);
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
+            this.RightToLeftLayout = true;
+            this.Text = "Mewwwww🐈";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CatName;
+        private System.Windows.Forms.Button More;
+        private System.Windows.Forms.TextBox Say;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.RichTextBox ChatList;
+    }
+}
---
+++ C#作业/Mewwwww/Mewwwww/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace Mewwwww
+{
+    public partial class Home : Form
+    {
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new Home());
+        }
+
+        public Home()
+        {
+            InitializeComponent();
+            ChatUpdateCat("你好呀。我叫Mew，我是一只小傻猫~\n你叫什么名字呢？");
+        }
+
+        //载入图像（猫头像）
+        //Bitmap Head = new Bitmap("Images/Cat/ChatHead.bmp");
+        const String KEY = "98373f587e7b4ae4be9d34d1d580dcc3";
+        Encoding encoding = Encoding.GetEncoding("utf-8");
+
+
+        public static string HttpGet(string url, Encoding encoding)
+        {
+            string data = "";
+            try
+            {
+                WebRequest request; //使用url发出请求的类
+                request = WebRequest.Create(url);
+                request.Credentials = CredentialCache.DefaultCredentials;   //使用默认的身份验证
+                request.Timeout = 10000;    //设定超时
+                WebResponse response;   //提供响应的类
+                response = request.GetResponse();
+                data = new StreamReader(response.GetResponseStream(), encoding).ReadToEnd();    //获取数据
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return data;
+        }
+
+        class Player
+        {
+            public string Name = "铲屎官";
+            public bool isName = false;
+            public bool isStart = false;
+        }
+
+        Player Player0 = new Player();
+
+        //点击发送
+        private void Send_Click(object sender, EventArgs e)
+        {
+            ChatList.Text += "\n\n" + Player0.Name + ": " + Say.Text;
+            if (!Player0.isName)
+            {
+                Player0.isStart = true;
+                Player0.isName = true;
+                Player0.Name = Say.Text;
+                ChatUpdateCat("好的~以后可以叫你" + Player0.Name + "了喵~");
+            }
+            else if (Say.Text == "空明杖是小弱受！")
+                ChatUpdateCat("赞同+1哟！喵喵喵w");
+            //else
+            //ChatUpdateCat("目前只能识别两句话呀，体谅一下萌新开发者喵www");
+            else
+            {
+                string url = "http://www.tuling123.com/openapi/api?key=" + KEY + "&info=" + Say.Text;
+                String data = HttpGet(url, encoding);
+                JsonReader reader = new JsonTextReader(new StringReader(data));
+                while (reader.Read())
+                {
+                    //ChatList.AppendText(reader.TokenType + "\t\t" + reader.ValueType + "\t\t" + reader.Value + "\n");
+                    if (reader.TokenType == JsonToken.PropertyName
+                    && reader.ValueType == Type.GetType("System.String")
+                    && Convert.ToString(reader.Value) == "code")
+                    {
+                        /* 如果当前Value是code，读取下一条，查看code的值 */
+                        reader.Read();
+                        switch (Convert.ToInt32(reader.Value))
+                        {
+                            case 100000:
+                                /* 返回码为文本类数据 */
+                                reader.Read();
+                                reader.Read();
+                                ChatUpdateCat((string)reader.Value);
+                                break;
+                            default:
+                                ChatUpdateCat("暂时无法读取的消息类型，试试聊点别的喵w");
+                                break;
+                        }
+                    }
+                }
+            }
+            ToDown();
+            Say.Text = "";
+        }
+
+        //显示猫猫的话
+        private void ChatUpdateCat(string Said)
+        {
+            //CatName.Text = "< 对方正在输入...";
+            //Thread.Sleep(1500);    //TO DO: 这样写会导致整个程序休眠
+            //CatName.Text = "< Mewwwww";
+            ChatList.Text += "\n\n";
+            //Clipboard.SetDataObject(Head, false);
+            // ChatList.Paste();    //TO DO：没有做出来QAQ，头像在剪贴板里，但是没有显示
+            ChatList.Text += "Mew: ";
+            //ChatList.Select(ChatList.TextLength-5, 5);
+            //ChatList.SelectionColor = Color.Blue;    //TO DO: 为什么改不了颜色呢？？？
+            //ChatList.Select(ChatList.TextLength, 0);
+            ChatList.Text += Said;
+        }
+
+        //自动到底部
+        private void ToDown()
+        {
+            ChatList.Focus();    //获取焦点
+            ChatList.Select(ChatList.TextLength, 0);    //光标定位到文本最后
+            ChatList.ScrollToCaret();    //滚动到光标处
+            Say.Focus();
+        }
+
+        //TO DO:气泡聊天。可能用到：右对齐：ChatList.SelectionAlignment = HorizontalAlignment.Center;
+    }
+}
