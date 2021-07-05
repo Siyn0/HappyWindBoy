ddd103177dccbf48337e616cb953904533936caf
Siyn0 <36985807+Siyn0@users.noreply.github.com>
2021-07-05 16:33:47 +0800

    其实是没啥特殊的记事本

---
+++ C#作业/抽象圣经（记事本程序）/RapUndergroundKing/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace RapUndergroundKing
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新建ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.格式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.字体ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.物管ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TBox = new System.Windows.Forms.RichTextBox();
+            this.dialogOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.格式ToolStripMenuItem,
+            this.物管ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新建ToolStripMenuItem,
+            this.打开ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 新建ToolStripMenuItem
+            // 
+            this.新建ToolStripMenuItem.Name = "新建ToolStripMenuItem";
+            this.新建ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.新建ToolStripMenuItem.Text = "新建";
+            this.新建ToolStripMenuItem.Click += new System.EventHandler(this.新建ToolStripMenuItem_Click);
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.保存ToolStripMenuItem.Text = "保存为";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click_1);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 格式ToolStripMenuItem
+            // 
+            this.格式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.字体ToolStripMenuItem});
+            this.格式ToolStripMenuItem.Name = "格式ToolStripMenuItem";
+            this.格式ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.格式ToolStripMenuItem.Text = "格式";
+            // 
+            // 字体ToolStripMenuItem
+            // 
+            this.字体ToolStripMenuItem.Name = "字体ToolStripMenuItem";
+            this.字体ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.字体ToolStripMenuItem.Text = "字体和颜色";
+            this.字体ToolStripMenuItem.Click += new System.EventHandler(this.字体ToolStripMenuItem_Click_1);
+            // 
+            // 物管ToolStripMenuItem
+            // 
+            this.物管ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.帮助ToolStripMenuItem1});
+            this.物管ToolStripMenuItem.Name = "物管ToolStripMenuItem";
+            this.物管ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.物管ToolStripMenuItem.Text = "帮助";
+            // 
+            // 帮助ToolStripMenuItem1
+            // 
+            this.帮助ToolStripMenuItem1.Name = "帮助ToolStripMenuItem1";
+            this.帮助ToolStripMenuItem1.Size = new System.Drawing.Size(100, 22);
+            this.帮助ToolStripMenuItem1.Text = "关于";
+            this.帮助ToolStripMenuItem1.Click += new System.EventHandler(this.帮助ToolStripMenuItem1_Click);
+            // 
+            // TBox
+            // 
+            this.TBox.BackColor = System.Drawing.Color.White;
+            this.TBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TBox.Location = new System.Drawing.Point(12, 28);
+            this.TBox.Name = "TBox";
+            this.TBox.Size = new System.Drawing.Size(260, 222);
+            this.TBox.TabIndex = 1;
+            this.TBox.Text = "";
+            // 
+            // dialogOpenFile
+            // 
+            this.dialogOpenFile.FileName = "openFileDialog1";
+            this.dialogOpenFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            this.dialogOpenFile.FilterIndex = 2;
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.ShowColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.TBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "18031210224周静琪";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新建ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 物管ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem1;
+        private System.Windows.Forms.RichTextBox TBox;
+        private System.Windows.Forms.OpenFileDialog dialogOpenFile;
+        private System.Windows.Forms.ToolStripMenuItem 格式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 字体ToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ C#作业/抽象圣经（记事本程序）/RapUndergroundKing/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace RapUndergroundKing
+{
+    public partial class MainForm : Form
+    {
+        private string filename = "Untitled";
+        public MainForm(string filename)
+        {
+            InitializeComponent();
+            if(filename != null)
+            {
+                this.filename = filename;
+                OpenFile();
+            }
+        }
+
+
+        private void 新建ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TBox.Text = "";
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(dialogOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                filename = dialogOpenFile.FileName;
+                OpenFile();
+            }
+            
+        }
+
+        private void 保存ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveData = new SaveFileDialog();
+            SaveData.InitialDirectory = @"C:\";
+            SaveData.Filter = "txt文件(*.txt)|*.txt";
+            string TextName = "";
+
+            SaveData.FileName = "New";
+            if (SaveData.ShowDialog() == DialogResult.OK)
+            {
+                TextName = SaveData.FileName;
+            }
+            StreamWriter sw = new StreamWriter(SaveData.FileName);
+            sw.WriteLine(TBox.Text);
+            sw.Close();
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected void OpenFile()
+        {
+            try
+            {
+                TBox.Clear();
+                TBox.Text = File.ReadAllText(filename);
+            }
+            catch(IOException ex)
+            {
+                MessageBox.Show(ex.Message, "Simple Editor", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void 帮助ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("没错，就这么点功能了。如想体验全部记事本功能请充值会员，仅需5毛/秒！","开发者：z"); 
+        }
+
+
+        private void 字体ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TBox.SelectionFont = fontDialog1.Font;
+                TBox.SelectionColor = fontDialog1.Color;
+            }
+        }
+
+    }
+}
