332fb0be480c9dc42994bf42d5957fc0cf091b7d
Siyn0 <36985807+Siyn0@users.noreply.github.com>
2021-12-06 16:32:27 +0800

    生成的题目改为“xxx”+ “的” + 名词

--- LifeIsShort/TitleCreater/TitleCreater.py
+++ LifeIsShort/TitleCreater/TitleCreater.py
@@ -1,6 +1,6 @@
 #为了继续我们伟大的“每日一写”（又名：小学生写命题作文）工程，我写了这个题目生成器;
 #没啥算法可言，就写着玩的，不要在意;（python语法早忘光了，先看了好久入门教程，哈哈哈！）
-#思路是从分别存放名词和其他词语的两个txt文件里读到列表，然后随机选择一个名词和两个其他词，组成一个题目;
+#思路是从分别存放名词和其他词语的两个txt文件里读到列表，然后随机选择其他词+名词，组成一个题目;
 #每次生成题目后，可以输入0重新生成;
 #注：“每日一写”的作文目录在: https://github.com/Siyn0/HappyWindBoy/tree/master/ZZ_VanGay_Kmz
 
@@ -31,5 +31,5 @@ print("//----生成题目----//")
 
 End = "0"
 while(End != "1"):
-    print(random.choice(ThingList) + random.choice(CharacterList) + random.choice(ThingList))
+    print(random.choice(ThingList) + "的" + random.choice(CharacterList))
     End = input()
