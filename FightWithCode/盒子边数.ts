cbdcc34a5a3b61ff9f0ef478dc62bc84895c9605
Siyn0 <36985807+Siyn0@users.noreply.github.com>
2022-06-26 15:07:09 +0800

    Create 盒子边数.ts

---
+++ FightWithCode/盒子边数.ts
@@ -0,0 +1,15 @@
+/* 
+
+题目：https://www.codewars.com/kata/6129095b201d6b000e5a33f0/train/typescript
+
+Given a X*Y*Z box built by arranging 1*1*1 unit boxes, write a function f(X,Y,Z) that returns the number of edges (hence, boxlines) of length 1 (both inside and outside of the box)
+
+Notes
+Adjacent unit boxes share the same edges, so a 2*1*1 box will have 20 edges, not 24 edges
+X,Y and Z are strictly positive, and can go as large as 2^16 - 1
+
+题目大意：由1*1*1的盒子构成的大盒子有几条1长度的边。
+
+*/
+
+// 先挖个坑，待会再写
